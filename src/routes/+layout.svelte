ecc6c6d1c0359c4aedec85a5114ed3674760165a
Yanis <yanis.bmdn@gmail.com>
2024-10-20 19:34:20 +0900

    Add basic site and prepare for hosting on vercel

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import './app.css';
+	import './reset.css';
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+	<footer>Made with SvelteKit</footer>
+</div>
+
+<style>
+	footer {
+		text-align: center;
+	}
+	.app {
+        display: flex;
+        flex-direction: column;
+	}
+    main{
+        flex:1;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="content">
+    <h1>LogBook</h1>
+    <p>
+    For the next 6 months, i'll be logging my advancements everyday in this logbook.
+    Its content may be utterly random, but its goal is for me to have a record of my 
+    research and for you, readers, to have an insight on what I am working on and 
+    any interesting findings I may come upon. Although for now very basic, this logbook
+    will evolve throughout my stay to someday look more decent.
+    </p>
+    <p>
+    </p>
+
+    <h2>Sun. 10 - 20 - 2024</h2>
+    <p>
+    I have finally settled on a subject after 2 weeks of searching. Today, I just got this 
+    website running with very minimal content and styling. The logging will debut tomorrow !
+    As for now, here's just a simple website I stumbled upon which has pretty impressive
+    photography <a href="https://www.esa.int/ESA_Multimedia/Sets/Earth_from_Space_image_collection/(result_type)/images">ESA Earth from space</a>
+    </p>
+    
+</div>
