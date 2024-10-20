79faf392a69f50ae2a76a4c67c897a7c364d8d32
Yanis <yanis.bmdn@gmail.com>
2024-10-20 19:34:39 +0900

    Updates

---
+++ _app/immutable/entry/start.BziP101g.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CRRB87fX.js";export{t as start};
---
+++ _app/immutable/nodes/2.Tkn1RabF.js
@@ -0,0 +1,8 @@
+import{s as i,n as o}from"../chunks/scheduler.Bmg8oFKD.js";import{S as r,i as l,e as h,c as m,g as c,a as g,b as d,d as p}from"../chunks/index.D0kJly8Y.js";function u(n){let e,a=`<h1>LogBook</h1> <p>For the next 6 months, i&#39;ll be logging my advancements everyday in this logbook.
+    Its content may be utterly random, but its goal is for me to have a record of my 
+    research and for you, readers, to have an insight on what I am working on and 
+    any interesting findings I may come upon. Although for now very basic, this logbook
+    will evolve throughout my stay to someday look more decent.</p> <p></p> <h2>Sun. 10 - 20 - 2024</h2> <p>I have finally settled on a subject after 2 weeks of searching. Today, I just got this 
+    website running with very minimal content and styling. The logging will debut tomorrow !
+    As for now, here&#39;s just a simple website I stumbled upon which has pretty impressive
+    photography <a href="https://www.esa.int/ESA_Multimedia/Sets/Earth_from_Space_image_collection/(result_type)/images">ESA Earth from space</a></p>`;return{c(){e=h("div"),e.innerHTML=a,this.h()},l(t){e=m(t,"DIV",{class:!0,"data-svelte-h":!0}),c(e)!=="svelte-yb7lxh"&&(e.innerHTML=a),this.h()},h(){g(e,"class","content")},m(t,s){d(t,e,s)},p:o,i:o,o,d(t){t&&p(e)}}}class v extends r{constructor(e){super(),l(this,e,null,u,i,{})}}export{v as component};
